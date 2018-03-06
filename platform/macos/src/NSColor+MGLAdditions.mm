c32a8a6477371bbd5beee940a128a2aa65248165
Minh Nguyễn <mxn@1ec5.org>
2018-03-06 00:32:12 -0800

    [macos] Convert colors to sRGB for mbgl
    Assume mbgl needs colors in the sRGB color space rather than the calibrated RGB color space.

--- platform/macos/src/NSColor+MGLAdditions.mm
+++ platform/macos/src/NSColor+MGLAdditions.mm
@@ -6,14 +6,16 @@
 {
     CGFloat r, g, b, a;
 
-    [[self colorUsingColorSpaceName:NSCalibratedRGBColorSpace] getRed:&r green:&g blue:&b alpha:&a];
+    // The Mapbox Style Specification does not specify a color space, but it is
+    // assumed to be sRGB for consistency with CSS.
+    [[self colorUsingColorSpace:[NSColorSpace sRGBColorSpace]] getRed:&r green:&g blue:&b alpha:&a];
 
     return { (float)r, (float)g, (float)b, (float)a };
 }
 
 + (NSColor *)mgl_colorWithColor:(mbgl::Color)color
 {
-    return [NSColor colorWithCalibratedRed:color.r green:color.g blue:color.b alpha:color.a];
+    return [NSColor colorWithRed:color.r green:color.g blue:color.b alpha:color.a];
 }
 
 - (mbgl::style::PropertyValue<mbgl::Color>)mgl_colorPropertyValue
