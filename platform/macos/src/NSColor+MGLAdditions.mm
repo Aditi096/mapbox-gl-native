f2753caeec50ee7787a64c9ee25459df19bfe357
Minh Nguyễn <mxn@1ec5.org>
2018-03-06 01:49:46 -0800

    [macos] Use calibrated RGB on Sierra or below

--- platform/macos/src/NSColor+MGLAdditions.mm
+++ platform/macos/src/NSColor+MGLAdditions.mm
@@ -1,21 +1,31 @@
 #import "NSColor+MGLAdditions.h"
 
+#import <Availability.h>
+
 @implementation NSColor (MGLAdditions)
 
 - (mbgl::Color)mgl_color
 {
     CGFloat r, g, b, a;
 
+#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 101300
     // The Mapbox Style Specification does not specify a color space, but it is
     // assumed to be sRGB for consistency with CSS.
     [[self colorUsingColorSpace:[NSColorSpace sRGBColorSpace]] getRed:&r green:&g blue:&b alpha:&a];
+#else
+    [[self colorUsingColorSpaceName:NSCalibratedRGBColorSpace] getRed:&r green:&g blue:&b alpha:&a];
+#endif
 
     return { (float)r, (float)g, (float)b, (float)a };
 }
 
 + (NSColor *)mgl_colorWithColor:(mbgl::Color)color
 {
+#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 101300
     return [NSColor colorWithRed:color.r green:color.g blue:color.b alpha:color.a];
+#else
+    return [NSColor colorWithCalibratedRed:color.r green:color.g blue:color.b alpha:color.a];
+#endif
 }
 
 - (mbgl::style::PropertyValue<mbgl::Color>)mgl_colorPropertyValue
